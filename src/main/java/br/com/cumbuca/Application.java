e56004894b9dd87fb3e98ed6417f6fa9a974ee8c
rayanne-on <rayanne.oliveira.bento@ccc.ufcg.edu.br>
2025-07-31 22:53:02 -0300

    feat: conecta aplicação ao banco de dados postgresql

--- src/main/java/br/com/cumbuca/Application.java
+++ src/main/java/br/com/cumbuca/Application.java
@@ -1,11 +1,20 @@
 package br.com.cumbuca;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
+        System.setProperty("DB_USER", Objects.requireNonNull(dotenv.get("DB_USER")));
+        System.setProperty("DB_PASS", Objects.requireNonNull(dotenv.get("DB_PASS")));
+
         SpringApplication.run(Application.class, args);
     }
 }
