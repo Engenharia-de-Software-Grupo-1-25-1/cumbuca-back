4a7e87dd5a6e23150d7fbd744064e1e3cf279a03
rayanne-on <rayanne.oliveira.bento@ccc.ufcg.edu.br>
2025-08-03 16:58:29 -0300

    refactor: remove anotações desnecessárias

--- src/main/java/br/com/cumbuca/dto/login/LoginRequestDTO.java
+++ src/main/java/br/com/cumbuca/dto/login/LoginRequestDTO.java
@@ -1,15 +1,9 @@
 package br.com.cumbuca.dto.login;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class LoginRequestDTO {
 
     @NotBlank
