1a604c413f72ef2345376f865745607a0021a1d8
helenazbm <maria.helena.barros.moraes@ccc.ufcg.edu.br>
2025-08-07 01:45:34 -0300

    feat: adiciona controller de avaliacao e endpoint de criacao

--- src/main/java/br/com/cumbuca/controller/AvaliacaoController.java
+++ src/main/java/br/com/cumbuca/controller/AvaliacaoController.java
@@ -1,13 +1,34 @@
 package br.com.cumbuca.controller;
 
+import br.com.cumbuca.dto.avaliacao.AvaliacaoRequestDTO;
+import br.com.cumbuca.dto.avaliacao.AvaliacaoResponseDTO;
+import br.com.cumbuca.model.Avaliacao;
+import br.com.cumbuca.service.avaliacao.AvaliacaoService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/avaliacao")
 public class AvaliacaoController {
 
+    private final AvaliacaoService avaliacaoService;
+
+    public AvaliacaoController(AvaliacaoService avaliacaoService) {this.avaliacaoService = avaliacaoService;}
+
+    @PostMapping("/criar")
+    public ResponseEntity<AvaliacaoResponseDTO> criar(
+            @ModelAttribute @Valid AvaliacaoRequestDTO avaliacaoRequestDTO,
+            UriComponentsBuilder uriBuilder) {
+        final Avaliacao avaliacao = avaliacaoService.criar(avaliacaoRequestDTO);
+        final URI uri = uriBuilder
+                .path("/avaliacao/{id}")
+                .buildAndExpand(avaliacao.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(new AvaliacaoResponseDTO(avaliacao));
+    }
 }
