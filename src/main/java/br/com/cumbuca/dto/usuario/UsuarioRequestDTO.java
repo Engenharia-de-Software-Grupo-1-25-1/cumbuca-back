8856fef9081b34429325e851129f5dfdede55c55
helenazbm <maria.helena.barros.moraes@ccc.ufcg.edu.br>
2025-08-09 13:32:34 -0300

    chore: retorna com anotacoes de notNull e notBlank

--- src/main/java/br/com/cumbuca/dto/usuario/UsuarioRequestDTO.java
+++ src/main/java/br/com/cumbuca/dto/usuario/UsuarioRequestDTO.java
@@ -1,6 +1,8 @@
 package br.com.cumbuca.dto.usuario;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,14 +11,19 @@ import java.time.LocalDate;
 @Data
 public class UsuarioRequestDTO {
 
+    @NotBlank
     private String email;
 
+    @NotBlank
     private String senha;
 
+    @NotBlank
     private String nome;
 
+    @NotBlank
     private String username;
 
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dtNascimento;
 
