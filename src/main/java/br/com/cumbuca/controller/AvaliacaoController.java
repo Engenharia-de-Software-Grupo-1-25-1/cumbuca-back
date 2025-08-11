51342cbfa217e5d1a96201c378fc14b9b38970a5
Luciano Nascimento <luciano.souza.nascimento.filho@ccc.ufcg.edu.br>
2025-08-11 12:43:07 -0300

    fix: corrige pipeline

--- src/main/java/br/com/cumbuca/controller/AvaliacaoController.java
+++ src/main/java/br/com/cumbuca/controller/AvaliacaoController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.net.URI;
 
