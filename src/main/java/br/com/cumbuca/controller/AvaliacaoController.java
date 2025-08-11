f3c0a217025da20f44018afebb610bdbffa63aa8
Luciano Nascimento <luciano.souza.nascimento.filho@ccc.ufcg.edu.br>
2025-08-11 12:52:27 -0300

    fix: corrige pipeline

--- src/main/java/br/com/cumbuca/controller/AvaliacaoController.java
+++ src/main/java/br/com/cumbuca/controller/AvaliacaoController.java
@@ -6,7 +6,14 @@ import br.com.cumbuca.model.Avaliacao;
 import br.com.cumbuca.service.avaliacao.AvaliacaoService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
