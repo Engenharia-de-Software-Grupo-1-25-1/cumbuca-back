edf1782dfc32566585548284ddefae84099f8f4d
ViniciusI4n <vinicius.ian.alves.lopes@ccc.ufcg.edu.br>
2025-08-11 22:47:21 -0300

    chore: ajusta checkstyle

--- src/main/java/br/com/cumbuca/config/ModelMapperConfig.java
+++ src/main/java/br/com/cumbuca/config/ModelMapperConfig.java
@@ -3,7 +3,6 @@ package br.com.cumbuca.config;
 import br.com.cumbuca.dto.avaliacao.AvaliacaoRequestDTO;
 import br.com.cumbuca.model.Avaliacao;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +12,12 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper mm = new ModelMapper();
+        final ModelMapper mm = new ModelMapper();
         mm.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT).setSkipNullEnabled(true);
 
         mm.addConverter(ctx -> {
-            AvaliacaoRequestDTO s = ctx.getSource();
-            Avaliacao d = new Avaliacao();
+            final AvaliacaoRequestDTO s = ctx.getSource();
+            final Avaliacao d = new Avaliacao();
             d.setItemConsumido(s.getItemConsumido());
             d.setPreco(s.getPreco());
             d.setDescricao(s.getDescricao());
