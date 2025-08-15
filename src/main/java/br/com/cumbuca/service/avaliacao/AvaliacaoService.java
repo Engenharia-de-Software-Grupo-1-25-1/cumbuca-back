04747e92b3a21375188236f66fa4d9f74c1f9a0d
Thawanny Fernandes <thawany.karoline.sousa.fernandes@ccc.ufcg.edu.br>
2025-08-15 13:00:20 -0300

    Implementa detalhar avaliação

--- src/main/java/br/com/cumbuca/service/avaliacao/AvaliacaoService.java
+++ src/main/java/br/com/cumbuca/service/avaliacao/AvaliacaoService.java
@@ -1,10 +1,12 @@
 package br.com.cumbuca.service.avaliacao;
 
 import br.com.cumbuca.dto.avaliacao.AvaliacaoRequestDTO;
+import br.com.cumbuca.dto.avaliacao.AvaliacaoResponseDTO;
 import br.com.cumbuca.model.Avaliacao;
 
 public interface AvaliacaoService {
     Avaliacao criar(AvaliacaoRequestDTO avaliacaoRequestDTO);
     Avaliacao atualizar(Long id, AvaliacaoRequestDTO dto);
     void remover(Long id);
+    AvaliacaoResponseDTO buscarAvaliacao(Long id);
 }
--- src/main/java/br/com/cumbuca/service/avaliacao/AvaliacaoServiceImpl.java
+++ src/main/java/br/com/cumbuca/service/avaliacao/AvaliacaoServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.cumbuca.service.avaliacao;
 
 import br.com.cumbuca.dto.avaliacao.AvaliacaoRequestDTO;
+import br.com.cumbuca.dto.avaliacao.AvaliacaoResponseDTO;
 import br.com.cumbuca.exception.CumbucaException;
 import br.com.cumbuca.model.Avaliacao;
 import br.com.cumbuca.model.Estabelecimento;
@@ -105,4 +106,11 @@ public class AvaliacaoServiceImpl implements AvaliacaoService {
 
         avaliacaoRepository.delete(avaliacao);
     }
+    @Override
+    public AvaliacaoResponseDTO buscarAvaliacao(Long id){
+        Avaliacao avaliacao = avaliacaoRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Avaliação não encontrada"));
+        return  new AvaliacaoResponseDTO(avaliacao);
+    }
+
 }
