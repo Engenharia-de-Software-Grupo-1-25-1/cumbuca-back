3a267bc753958510978f44899299130291cbaf50
marciathais <marcia.thais.revoredo.aires@ccc.ufcg.edu.br>
2025-08-23 14:04:34 -0300

    ajusta pipeline

--- src/main/java/br/com/cumbuca/controller/AvaliacaoController.java
+++ src/main/java/br/com/cumbuca/controller/AvaliacaoController.java
@@ -5,9 +5,15 @@ import br.com.cumbuca.dto.avaliacao.AvaliacaoResponseDTO;
 import br.com.cumbuca.service.avaliacao.AvaliacaoService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
