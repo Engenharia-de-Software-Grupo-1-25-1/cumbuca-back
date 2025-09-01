f1ac8959a2513930d0ac47a6b7480d6b9b9acfe7
ViniciusI4n <vinicius.ian.alves.lopes@ccc.ufcg.edu.br>
2025-09-01 00:15:52 -0300

    chore: deploy

--- src/main/java/br/com/cumbuca/Application.java
+++ src/main/java/br/com/cumbuca/Application.java
@@ -9,17 +9,6 @@ import java.util.Objects;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        final Dotenv dotenv = Dotenv.configure().load();
-
-        System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
-        System.setProperty("DB_USER", Objects.requireNonNull(dotenv.get("DB_USER")));
-        System.setProperty("DB_PASS", Objects.requireNonNull(dotenv.get("DB_PASS")));
-
-        System.setProperty("JWT_SECRET", Objects.requireNonNull(dotenv.get("JWT_SECRET")));
-        System.setProperty("JWT_EXPIRATION_MINUTES", Objects.requireNonNull(dotenv.get("JWT_EXPIRATION_MINUTES")));
-
-        System.setProperty("MAIL_PASSWORD", Objects.requireNonNull(dotenv.get("MAIL_PASSWORD")));
-
         SpringApplication.run(Application.class, args);
     }
 }
