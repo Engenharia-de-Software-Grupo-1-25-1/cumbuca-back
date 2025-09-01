b18ea56ecce49d622fac80b0435fef61340c99cf
ViniciusI4n <vinicius.ian.alves.lopes@ccc.ufcg.edu.br>
2025-09-01 01:30:53 -0300

    chore: ajusta cors

--- src/main/java/br/com/cumbuca/security/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/cumbuca/security/ConfiguracoesSeguranca.java
@@ -31,7 +31,7 @@ public class ConfiguracoesSeguranca {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://cumbuca.onrender.com "));
+        config.setAllowedOrigins(List.of("https://cumbuca.onrender.com "));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
